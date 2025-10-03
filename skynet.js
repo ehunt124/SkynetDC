4ac8c8a02848a46a556cf049635db617ad73b60c
ehunt124 <ehunt124@terpmail.umd.edu>
2025-10-03 17:51:47 -0400

    Customized initial site with flexbox support

--- skynet.js
+++ skynet.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.registerPlugin(ScrollTrigger);
 
-    var frame_count  = 40, offset_value = 190;
+    var frame_count  = 39, offset_value = 190;
 
     gsap.to(".viewer", {
     backgroundPosition: (-offset_value * frame_count * 2) + "px 50%",
-    ease: "steps(" + frame_count + ")", // use a stepped ease for the sprite sheet
+    ease: "steps(" + frame_count + ")",
     scrollTrigger: {
         trigger: ".scene",
         start: "top top",
-        end: "+=" + (frame_count * offset_value),
+        end: "+=" + (frame_count* offset_value),
         pin: true,
         scrub: true
     }
